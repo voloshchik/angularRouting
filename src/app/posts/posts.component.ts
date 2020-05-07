5d0cea9c371cf020494c26de9739c3a3ef0da5f9
vladimir <lovkiy2012@gmail.com>
2020-05-07 23:35:37 +0300

    84 work with params

--- src/app/posts/posts.component.ts
+++ src/app/posts/posts.component.ts
@@ -1,11 +1,35 @@
-import {Component} from '@angular/core'
-import {PostsService} from '../posts.service'
+import { Component, OnInit } from "@angular/core";
+import { PostsService } from "../posts.service";
+import { Router } from "@angular/router";
+import { ActivatedRoute, Params } from "@angular/router";
 
 @Component({
-  selector: 'app-posts',
-  templateUrl: './posts.component.html',
-  styleUrls: ['./posts.component.scss']
+  selector: "app-posts",
+  templateUrl: "./posts.component.html",
+  styleUrls: ["./posts.component.scss"],
 })
-export class PostsComponent {
-  constructor(private postsService: PostsService) {}
+export class PostsComponent implements OnInit {
+  showIds = false;
+  constructor(
+    private postsService: PostsService,
+    private route: ActivatedRoute,
+    private router: Router
+  ) {}
+  ngOnInit() {
+    this.route.queryParams.subscribe((params: Params) => {
+      this.showIds = !!params.showIds;
+      console.log("params", params);
+    });
+    this.route.fragment.subscribe((fragment)=>{
+      console.log('fragment', fragment)
+    })
+  }
+  showIdsPrograms() {
+    this.router.navigate(["posts"], {
+      queryParams: {
+        showIds: true,
+      },
+      fragment:'jjjjjj'
+    });
+  }
 }
