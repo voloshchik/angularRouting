0798df06b05fd7635e206363a7cfd3bc5fe4827a
vladimir <lovkiy2012@gmail.com>
2020-05-09 14:51:09 +0300

    88 protection children routers

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,21 +1,26 @@
-import { ErrorPageComponent } from './error-page/error-page.component';
-import { AboutExtraComponent } from './about-extra/about-extra.component';
+import { ErrorPageComponent } from "./error-page/error-page.component";
+import { AboutExtraComponent } from "./about-extra/about-extra.component";
 import { AboutComponent } from "./about/about.component";
 import { HomeComponent } from "./home/home.component";
-import { PostsComponent } from './posts/posts.component';
-import { PostComponent } from './post/post.component';
+import { PostsComponent } from "./posts/posts.component";
+import { PostComponent } from "./post/post.component";
 
 import { NgModule } from "@angular/core";
 import { RouterModule, Routes } from "@angular/router";
-import { AuthGuard } from './auth.guard';
+import { AuthGuard } from "./auth.guard";
 
 const routes: Routes = [
   { path: "", component: HomeComponent },
-  { path: "about", component: AboutComponent,children:[{path:'extra',component:AboutExtraComponent}] },
-  { path: "posts", component: PostsComponent ,canActivate:[AuthGuard]},
+  {
+    path: "about",
+    component: AboutComponent,
+    canActivateChild: [AuthGuard],
+    children: [{ path: "extra", component: AboutExtraComponent }],
+  },
+  { path: "posts", component: PostsComponent, canActivate: [AuthGuard] },
   { path: "posts/:id", component: PostComponent },
-  {path:"error",component:ErrorPageComponent},
-  {path:"**",redirectTo:"error"}
+  { path: "error", component: ErrorPageComponent },
+  { path: "**", redirectTo: "error" },
 ];
 
 @NgModule({
--- src/app/auth.guard.ts
+++ src/app/auth.guard.ts
@@ -3,16 +3,15 @@ import {
   CanActivate,
   ActivatedRouteSnapshot,
   RouterStateSnapshot,
-  Router
+  Router,
+  CanActivateChild,
 } from "@angular/router";
 import { Observable } from "rxjs";
 import { Injectable } from "@angular/core";
 
 @Injectable({ providedIn: "root" })
-export class AuthGuard implements CanActivate {
-  constructor(private authService: AuthServece,
-    private router:Router
-    ) {}
+export class AuthGuard implements CanActivate, CanActivateChild {
+  constructor(private authService: AuthServece, private router: Router) {}
   canActivate(
     route: ActivatedRouteSnapshot,
     state: RouterStateSnapshot
@@ -20,13 +19,20 @@ export class AuthGuard implements CanActivate {
     return this.authService.isAuthenticated().then((isAuth) => {
       if (isAuth) {
         return true;
-      }else{
-this.router.navigate(['/'],{
-    queryParams:{
-        auth:false
-    }
-})
+      } else {
+        this.router.navigate(["/"], {
+          queryParams: {
+            auth: false,
+          },
+        });
       }
     });
+  }
+
+  canActivateChild(
+    route: ActivatedRouteSnapshot,
+    state: RouterStateSnapshot
+  ): Observable<boolean> | Promise<boolean> | boolean {
+    return this.canActivate(route, state);
   }
 }
